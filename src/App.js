9dc2e15b2d806d65605af39fc990e23527421280
Tasha Perry <nata1031986@gmail.com>
2024-02-15 11:05:35 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+    <div className="container">
       <header className="App-header"></header>
-      
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        Coded by Tasha
+      </footer>
     </div>
   );
 }
