37633f9f676ca1137cbb6008aa9d2f7897e60cfb
Tasha Perry <nata1031986@gmail.com>
2024-02-15 11:17:11 +0200

    fg

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Dictionary.css";
 export default function Dictionary() {
-    return 
-        <div className="Dictionary">
-            Hello from Dictionary</div>
-    
+    return (
+    <div className="Dictionary">
+           <form>
+            <input type="search" />
+           </form>
+            
+            
+            </div>
+            
+        );
 }
