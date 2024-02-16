e7cd01d743829e35f007d7ab85126698d94ce151
Tasha Perry <nata1031986@gmail.com>
2024-02-16 14:46:35 +0200

    ttdff

--- src/Results.js
+++ src/Results.js
@@ -1,23 +1,29 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic"; // Ensure this is imported correctly
 
 export default function Results(props) {
-    if (props.results) {
-        return (
-            <div className="Results">
-                <h2>{props.results.word}</h2>
-                {props.results.phonetics && props.results.phonetics.map((phonetic, index) => (
-                    <Phonetic key={index} phonetic={phonetic} />
-                ))}
-                {props.results.meanings && props.results.meanings.map((meaning, index) => (
-                    <div key={index}>
-                        <Meaning meaning={meaning} />
-                    </div>
-                ))}
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
             </div>
-        );
-    } else {
-        return null;
-    }
+          );
+        })}
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
