5fc09010e94762be62c272a15f3b3d870d8824ef
Tasha Perry <nata1031986@gmail.com>
2024-02-17 23:12:58 +0200

    rt

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,34 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 
 export default function Dictionary() {
     const [keyword, setKeyword] = useState("");
     const [results, setResults] = useState(null);
+    const [photos, setPhotos] = useState(null);
 
+    function handlePexelsResponse(response) {
+        console.log(response.data);
+    }
+function handleDectionaryResponse (response) {
+    setResults(response.data[0]);
+}
     function search(event) {
         event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl)
-            .then(handleResponse)
-            .catch(handleError);
+        let headers ={ Authorization: `Bearer ${pexelsApiKey}`};
+        axios.get(apiUrl, {headers: headers}).then(handlePexelsResponse);
+    }
+
+            let pexelsApiKey = 
+            "843fed47f3a58c43ftc397aabb8d5oc0";
+            let pexelApiUrl = 
+            `https://api.shecodes.io/images/v1/search?query=${keyword}&per_page=6`;
+            axios.get(pexelsApiUrl).then(handlePexelsResponse);
     }
 
     function handleResponse(response) {
@@ -41,6 +55,7 @@ export default function Dictionary() {
                 <input type="search" onChange={handleKeywordChange} />
             </form>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     );
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,8 +5,9 @@ export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        Listen
+      <a href={props.phonetic.audio} target="_blank"
+      rel="noreferrer">
+       AUDIO
       </a>
    
       {props.phonetic.text}
